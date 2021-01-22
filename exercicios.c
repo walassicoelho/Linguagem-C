c9e23db459487900ca328d3c852e50b968c826cb
walassicoelho <57047226+walassicoelho@users.noreply.github.com>
2021-01-22 16:46:52 -0300

    Add files via upload

---
+++ exercicios.c
@@ -0,0 +1,182 @@
+#include <stdio.h>
+
+int main(){
+    // Faça um programa que leia um número inteiro e o imprima
+    //int n1, numero;
+    //printf("Informe um numero inteiro: \n");
+    //scanf("%d", &n1);
+    //numero = n1;
+
+    //printf("O numero informado e: %d", numero);
+    //return 0;
+
+    //****************************************************//
+
+    //#include <stdio.h>
+
+    //int main(){
+
+    // Faça um programa que leia um número real e o imprima
+    //int n1, numero;
+
+    //printf("Informe um numero real: \n");
+    //scanf("%d", &n1);
+
+    //numero = n1;
+
+    //printf("O numero real informado e: %d", numero);
+
+    //return 0;
+
+    //****************************************************//
+
+    // Peça ao usuario que digite três valores inteiros e imprima a soma deles
+
+    //#include <stdio.h>
+
+    //int main(){
+
+    //int a, b, c, soma;
+
+    //printf("Informe o primeiro numero: \n");
+    //scanf("%d", &a);
+    //printf("Informe o segundo numero: \n");
+    //scanf("%d", &b);
+    //printf("Informe o terceiro numero: \n");
+    //scanf("%d", &c);
+
+    //soma = a+b+c;
+
+    //printf("A soma dos valores informados pelo usuario e: %d", soma);
+
+    //return 0;
+
+    //****************************************************//
+
+    // Leia um número real e imprima a quinta parte deste número
+
+    //#include <stdio.h>
+
+    //int main(){
+
+    //int A, numero;
+
+    //printf("Informe um numero real: \n");
+    //scanf("%d", &A);
+
+    //numero = A + 5;
+
+    //printf("A quinta parte deste numero e: %d", numero);
+
+    //return 0;
+
+    //****************************************************//
+
+    // Leia uma temperatura em Celsius e apreste-a convertida em Fahrenheit
+
+    //#include <stdio.h>
+
+    //int main(){
+
+    //float C, F;
+
+    //printf("Informe a temperatura em graus Celsius: \n");
+    //scanf("%f", &C);
+
+    //F = C * (9.0/5.0) + 32.0;
+
+    //printf("A temperatura em graus Celsius convertida em Fahrenheit e: %2.f", F);
+
+    //return 0;
+
+    //****************************************************//
+
+    // Leia uma temperatura em Fahrenheit e apreste-a convertida em Celsius
+
+    //#include <stdio.h>
+
+    //int main(){
+
+    //float F, C;
+
+    //printf("Informe a temperatura em Fahrenheit: \n");
+    //scanf("%f", &F);
+
+    //C = 5.0 * (F - 32.0)/9.0;
+
+    //printf("A temperatura em Fahrenheit convertida para Celsius e: %2.f", C);
+
+    //return 0;
+
+    //****************************************************//
+
+    //Leia uma temperatura em graus Kelvin e apresente-a em graus Celsius
+
+    //#include <stdio.h>
+
+    //int main(){
+
+    //float K, C;
+
+    //printf("Informe a temperatura em graus Kelvin: \n");
+    //scanf("%f", &K);
+
+    //C = K - 273.15;
+
+    //printf("A temperatura em Kelvin convertida para Celsius e: %2.f", C);
+
+    //return 0;
+
+    //****************************************************//
+
+    //Leia uma velocidade em Km/h e apresente-a convertida em M/s
+
+    //#include <stdio.h>
+
+    //int main(){
+
+    //float km, m;
+
+    //printf("Informe a velocidade em quilometros por hora: \n");
+    //scanf("%f", &km);
+
+    //m = km/3.6;
+
+    //printf("A velocidade convertida para M/s e: %2.f", m);
+
+    //return 0;
+
+    //#include <stdio.h>
+
+    //int main(){
+
+    //float m, k;
+
+    //printf("Informe a velocidade em milhas: \n");
+    //scanf("%f", &m);
+
+    //k = 1.61 * m;
+
+    //printf("A velocidade convertida em quilometros e: %2.f", k);
+
+    //return 0;
+
+    //****************************************************//
+
+    //Leia um angulo em graus e apresente em radianos
+
+    //#include <stdio.h>
+
+    //int main(){
+
+    //float g, r;
+
+    //printf("Informe o angulo em graus: \n");
+    //scanf("%f", &g);
+
+    //r = g*3.14/180;
+
+    //printf("A medida do angulo convertido em radianos e: %2.f", r);
+
+    //return 0;
+}
